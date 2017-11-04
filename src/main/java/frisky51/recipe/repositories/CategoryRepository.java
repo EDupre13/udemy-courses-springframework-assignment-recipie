7cb7925f086a3815dbd361450d539b477f4c18d2
EDupre13 <EDupre13@gmail.com>
2017-11-04 10:51:15 -0500

    Lecture 103

--- src/main/java/frisky51/recipe/repositories/CategoryRepository.java
+++ src/main/java/frisky51/recipe/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package frisky51.recipe.repositories;
 import frisky51.recipe.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/frisky51/recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/frisky51/recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package frisky51.recipe.repositories;
 import frisky51.recipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
