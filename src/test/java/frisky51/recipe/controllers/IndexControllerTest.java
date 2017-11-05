a091ff770932aedb3b0f3c12ec81070c50f1540f
EDupre13 <EDupre13@gmail.com>
2017-11-05 15:06:49 -0600

    Lecture 124 - mockito argument capture

--- src/test/java/frisky51/recipe/controllers/IndexControllerTest.java
+++ src/test/java/frisky51/recipe/controllers/IndexControllerTest.java
@@ -1,17 +1,23 @@
 package frisky51.recipe.controllers;
 
+import frisky51.recipe.domain.Recipe;
 import frisky51.recipe.services.IRecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class IndexControllerTest {
 
@@ -30,10 +36,28 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() throws Exception {
+
+        // Given that
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(4L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // When
         String indexPage = indexController.getIndexPage(model);
 
+        // This is true
         assertEquals("index", indexPage);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        Set<Recipe> setInCotroller = argumentCaptor.getValue();
+        assertEquals(2, setInCotroller.size());
     }
 }
